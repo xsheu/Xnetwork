3fb0f2c628dd2c3a773eafa578b78319801c09d2
Guang Yih Sheu <xsheu@hotmail.com>
2019-06-22 18:53:18 +0800

    2019 6/22 18:53

---
+++ Xnetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xnetwork
+//
+//  Created by 許光毅 on 2019/5/27.
+//  Copyright © 2019 CJCU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
